c7d6c9c955d36749bd408003f94631bd99831228
murugaraja <murugarajamca@gmail.com>
2016-09-15 14:58:48 +0530

    Service and run page

---
+++ Service/com.example.e4.rcp.todo.services/src/com/vogella/osgi/firstbundle/Activator.java
@@ -0,0 +1,22 @@
+package com.vogella.osgi.firstbundle;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import com.vogella.osgi.firstbundle.internal.MyThread;
+
+public class Activator implements BundleActivator {
+
+	private MyThread myThread;
+
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Starting com.vogella.osgi.firstbundle");
+		myThread = new MyThread();
+		myThread.start();
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Stopping com.vogella.osgi.firstbundle");
+		myThread.stopThread();
+		myThread.join();
+	}
+}
